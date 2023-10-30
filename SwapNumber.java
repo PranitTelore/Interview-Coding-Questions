6b17af33d8f55b38130f87d84bc9abcd64a4ac00
PranitTelore <pranit.telore1@gmail.com>
2023-10-30 16:49:06 +0530

    updated using third variable also

--- SwapNumber.java
+++ SwapNumber.java
@@ -11,5 +11,15 @@ public class SwapNumber {
         x = x + y;
         System.err.println("x=" + x + "  y=" + y);
 
+        // Using Third Variable
+        int temp;
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.err.println("Again Swapping using third variable :");
+        System.err.println("x=" + x + "  y=" + y);
+
     }
 }
